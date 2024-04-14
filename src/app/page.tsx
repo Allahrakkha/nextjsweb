eca15c84cc14f1e534b3e9a04f0fd2cfaa8a5ca5
ALLAH RAKKHA <150431493+Allahrakkha@users.noreply.github.com>
2024-04-14 11:56:22 +0500

    fourth commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import Image from "next/image";
+import NavBar from "./component/navbar";
 
 export default function Home() {
   return (
     <div>
       <h1>My porfolio web</h1>
+      <NavBar/>
     </div>
   );
 }
