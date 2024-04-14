a185870dca80e0be1c168dc485777de742432429
ALLAH RAKKHA <150431493+Allahrakkha@users.noreply.github.com>
2024-04-14 16:46:52 +0500

    spacing

--- src/app/component/navbar.tsx
+++ src/app/component/navbar.tsx
@@ -5,7 +5,7 @@ import Image from "next/image";
 const NavBar =()=>{
     return(
         <div><header className="text-gray-600 body-font">
-        <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
+        <div className="container mx-5 flex flex-wrap p-5 flex-col md:flex-row items-center">
           <a className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
        <Image src={require("../../../public/assesst/pictures/Allah Rakkha Pic.png")} alt="Allahrakkha" width={100} height={100}/>
             <span className="ml-3 text-xl">Allah Rakkha Portfolio Web</span>
